68856e54e65d631637ab34eaaa8ff7b1366f6b00
Robert Young <david41596@yahoo.com>
2025-09-22 01:31:06 -0400

    Started development and able to CRUD cabins from SUPABASE.

---
+++ src/features/cabins/CreateCabinForm.jsx
@@ -0,0 +1,139 @@
+import { useForm } from "react-hook-form";
+import { useCreateCabin } from "./useCreateCabin";
+import { useEditCabin } from "./useEditCabin";
+
+import Input from "../../ui/Input";
+import Form from "../../ui/Form";
+import Button from "../../ui/Button";
+import FileInput from "../../ui/FileInput";
+import Textarea from "../../ui/Textarea";
+import FormRow from "../../ui/FormRow";
+
+function CreateCabinForm({ cabinToEdit = {} }) {
+  const { id: editId, ...editValues } = cabinToEdit;
+  const isEditSession = Boolean(editId);
+
+  const { register, handleSubmit, reset, getValues, formState } = useForm({
+    defaultValues: isEditSession ? editValues : {},
+  });
+  const { errors } = formState;
+  const { isCreating, createCabin } = useCreateCabin();
+  const { isEditing, editCabin } = useEditCabin();
+
+  function onSubmit(data) {
+    console.log(data.image);
+    const image = typeof data.image === "string" ? data.image : data.image[0];
+
+    if (isEditSession) {
+      editCabin(
+        { newCabinData: { ...data, image }, id: editId },
+        { onSuccess: () => reset(getValues()) }
+      );
+    } else {
+      createCabin({ ...data, image: image }, { onSuccess: () => reset() });
+    }
+  }
+  function onError(error) {
+    console.log(error);
+  }
+  const isWorking = isCreating || isEditing;
+  return (
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+      <FormRow label="Cabin name" error={errors?.name?.message}>
+        <Input
+          type="text"
+          id="name"
+          disabled={isWorking}
+          {...register("name", {
+            required: "This field is required.",
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Maximum capacity" error={errors?.maxCapacity?.message}>
+        <Input
+          type="number"
+          id="maxCapacity"
+          onWheel={(e) => e.target.blur()}
+          disabled={isWorking}
+          {...register("maxCapacity", {
+            required: "This field is required.",
+            min: {
+              value: 1,
+              message: "Capacity should be at least 1.",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Regular price" error={errors?.regularPrice?.message}>
+        <Input
+          type="number"
+          id="regularPrice"
+          onWheel={(e) => e.target.blur()}
+          disabled={isWorking}
+          {...register("regularPrice", {
+            required: "This field is required.",
+            min: {
+              value: 1,
+              message: "Capacity should be at least 1.",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Discount" error={errors?.discount?.message}>
+        <Input
+          type="number"
+          id="discount"
+          defaultValue={0}
+          onWheel={(e) => e.target.blur()}
+          disabled={isWorking}
+          {...register("discount", {
+            required: "This field is required.",
+            validate: (value) =>
+              Number(value) <= Number(getValues().regularPrice) ||
+              "Discount cannot be more than the price.",
+          })}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Description for website"
+        error={errors?.description?.message}
+      >
+        <Textarea
+          type="number"
+          id="description"
+          defaultValue=""
+          disabled={isWorking}
+          {...register("description", {
+            required: "This field is required.",
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Cabin photo">
+        <FileInput
+          id="image"
+          accept="image/*"
+          disabled={isWorking}
+          {...register("image", {
+            required: isEditSession ? false : "This field is required.",
+          })}
+        />
+      </FormRow>
+
+      <FormRow>
+        <Button variation="secondary" type="reset">
+          Cancel
+        </Button>
+        <Button disabled={isWorking}>
+          {isEditSession ? "Edit Cabin" : "Create new cabin"}
+        </Button>
+      </FormRow>
+    </Form>
+  );
+}
+
+export default CreateCabinForm;
